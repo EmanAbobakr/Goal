136bae9d3208af609d97b40745c7ebfc9d469029
EmanAbobakr <43084491+EmanAbobakr@users.noreply.github.com>
2023-10-24 00:55:52 +0300

    split basic goal app into components

---
+++ components/GoalInput.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StyleSheet, View, TextInput, Button } from "react-native";
+
+export function GoalInput(props) {
+  const [text, setChangeText] = React.useState("");
+  const onChangeTxt = (txt) => {
+    console.log(txt);
+    setChangeText(txt);
+  };
+
+  const onClickBtn = (enteredGoalText) => {
+    props.pressBtn(text);
+    setChangeText("");
+  };
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.input} onChangeText={onChangeTxt} value={text} />
+      <Button title="Add Goal" onPress={onClickBtn} color="#237c94" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#808080",
+  },
+  input: {
+    width: "70%",
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    marginRight: 8,
+    padding: 8,
+    borderRadius: 6,
+  },
+});
